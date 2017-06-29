8e3b498862ff08581b711bd64bb70b294f0f2a21
Tianxiao Gu <tianxiao.gu@gmail.com>
2017-06-29 00:46:48 -0700

    Update

---
+++ sata/xpath-input-example/sataTimeline.vis.js
@@ -0,0 +1,13 @@
+var groups = new vis.DataSet();
+groups.add({id: 0, content: "SATA Action"});
+var items = new vis.DataSet([
+{start: new Date(1498721773630), title: "Thu Jun 29 00:36:13 PDT 2017", group: 0, screenURL: "step-1.png", id: 0, content: "LONG_CLICK", rect: [60,312,1038,393]},
+{start: new Date(1498721775504), title: "Thu Jun 29 00:36:15 PDT 2017", group: 0, screenURL: "step-2.png", id: 1, content: "CLICK", rect: [936,1575,1080,1719]},
+{start: new Date(1498721776282), title: "Thu Jun 29 00:36:16 PDT 2017", group: 0, screenURL: "step-3.png", id: 2, content: "CLICK", rect: [60,312,1038,393]},
+{start: new Date(1498721777154), title: "Thu Jun 29 00:36:17 PDT 2017", group: 0, screenURL: "step-4.png", id: 3, content: "CLICK", rect: [0,72,168,240]},
+{start: new Date(1498721777984), title: "Thu Jun 29 00:36:17 PDT 2017", group: 0, screenURL: "step-5.png", id: 4, content: "CLICK", rect: [504,1632,648,1776]},
+{start: new Date(1498721778876), title: "Thu Jun 29 00:36:18 PDT 2017", group: 0, screenURL: "step-6.png", id: 5, content: "CLICK", rect: [0,1575,144,1719]},
+{start: new Date(1498721779617), title: "Thu Jun 29 00:36:19 PDT 2017", group: 0, screenURL: "step-7.png", id: 6, content: "CLICK", rect: [60,312,912,393]},
+]);
+var startTime = new Date(1498721768333);
+var endTime = new Date(1498721780617);
